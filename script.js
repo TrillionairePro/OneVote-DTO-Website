d6967b5b0817529de248f23d8d0b1fc0e5d4abf5
TrillionairePro <t23.ong@student.stjohns-hamilton.school.nz>
2025-09-19 00:43:57 +0000

    OneVote
    Test

--- script.js
+++ script.js
@@ -17,17 +17,18 @@ function toggleDropdown(dropdownId) {
     dropdown.style.maxHeight = "0";
     arrow.style.transform = "rotate(0deg)";
     button.classList.remove('open');
-  } else {
-    const allDropdowns = document.querySelectorAll('.dropdown-content');
-    const allArrows = document.querySelectorAll('.dropdown-arrow');
-    const allButtons = document.querySelectorAll('.dropdown-toggle');
-    allDropdowns.forEach(dd => dd.style.maxHeight = "0");
-    allArrows.forEach(arr => arr.style.transform = "rotate(0deg)");
-    allButtons.forEach(btn => btn.classList.remove('open'));
-
-    dropdown.style.maxHeight = "200px";
-    arrow.style.transform = "rotate(180deg)";
-    button.classList.add('open');
+  }
+  
+  else {
+      const allDropdowns = document.querySelectorAll('.dropdown-content');
+      const allArrows = document.querySelectorAll('.dropdown-arrow');
+      const allButtons = document.querySelectorAll('.dropdown-toggle');
+      allDropdowns.forEach(dd => dd.style.maxHeight = "0");
+      allArrows.forEach(arr => arr.style.transform = "rotate(0deg)");
+      allButtons.forEach(btn => btn.classList.remove('open'));
+      dropdown.style.maxHeight = "200px";
+      arrow.style.transform = "rotate(180deg)";
+      button.classList.add('open');
   }
 }
 
